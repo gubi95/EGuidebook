9d1fa7151c33deaf3cfb1732f58e3fd2ae5a8440
Rafał Gubała <Rafa Gubaa>
2017-06-13 22:58:26 +0200

    Owin setup only

---
+++ EGuidebook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EGuidebook.MvcApplication" Language="C#" %>
---
+++ EGuidebook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EGuidebook.Startup))]
+namespace EGuidebook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
